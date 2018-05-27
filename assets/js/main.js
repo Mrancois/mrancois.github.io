fcf114f5a532f50f9ba9ab3d6c2d19fc8678835d
Unknown <fullmetal82@gmail.com>
2018-05-26 23:19:15 -0700

    refactor js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ function submitContact() {
   var subject = document.getElementById("subjectInput");
   var message = document.getElementById("messageInput");
 
-  var obj = {
+  var mail = {
     "name": name.value,
     "email": email.value,
     "subject": subject.value,
@@ -20,7 +20,7 @@ function submitContact() {
       "Access-Control-Allow-Credentials": true,
       "Content-Type": "application/json"
     },
-    body: JSON.stringify(obj)
+    body: JSON.stringify(mail)
   }).then(function(response) {
     var contactForm = document.getElementById("contact__form");
     contactForm.classList.add('no-display');
@@ -29,33 +29,40 @@ function submitContact() {
       var contactForm_success = document.getElementById("contact__form--success");
       contactForm_success.classList.remove('no-display');
     } else {
+      // ERROR
       var contactForm_error = document.getElementById("contact__form--error");
       contactForm_error.classList.remove('no-display');
     }
   });
 }
 
-
-
-
-
-
 function clickMenu(element) {
   var el = document.getElementById('sc' + element.innerHTML);
-  scrollIt(el)
+  scrollIt(el);
 }
 
 function scrollIt(element) {
+   // 55 (size_header) + 20 (margin_separator)
+  var offsetHeigh = (window.innerWidth >= 767) ? (55 - 20 ) : (83 - 20);
+
   window.scrollTo({
     'behavior': 'smooth',
     'left': 0,
-    'top': element.offsetTop - 75 // 55 (size of header) + 20 (margin separator)
+    'top': element.offsetTop - offsetHeigh // 55 (size of header) + 20 (margin separator)
   });
 }
 
 
-window.onload=function(){
+window.onload = function() {
 
+  [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+  img.setAttribute('src', img.getAttribute('data-src'));
+  img.onload = function() {
+      img.removeAttribute('data-src');
+    };
+  });
+
+  // init ISOTOPE
   var postElement = document.getElementById("post");
   if (postElement) {
     var iso = new Isotope('#post', {
@@ -64,7 +71,6 @@ window.onload=function(){
       isFitWidth: true
     });
   }
-
 
   var filtersElem = document.getElementById("filters");
   if (filtersElem) {
@@ -89,8 +95,6 @@ window.onload=function(){
     });
   }
 
-
-
   var email = document.getElementById("emailInput");
   var name = document.getElementById("nameInput");
   var subject = document.getElementById("subjectInput");
@@ -109,7 +113,3 @@ window.onload=function(){
     });
   }
 }
-
-window.addEventListener("resize", function(){
-  console.log('resize');
-});
